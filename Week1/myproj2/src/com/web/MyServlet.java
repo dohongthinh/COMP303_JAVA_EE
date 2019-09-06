333989cd365ffab83227e441f1d2e417c95e4ef4
dohongthinh <dohongthinhdht1999@gmail.com>
2019-09-06 10:07:27 -0400

    add html

--- Week1/myproj2/src/com/web/MyServlet.java
+++ Week1/myproj2/src/com/web/MyServlet.java
@@ -1,6 +1,8 @@
 package com.web;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -27,7 +29,13 @@ public class MyServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<html><body>");
+		out.println("<h1> This is my servlet page </h1>");
+		out.println("</body></html>");
+		out.close();
 	}
 
 	/**
