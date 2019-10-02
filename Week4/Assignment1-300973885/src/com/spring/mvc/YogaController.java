0f98bda753daa4f3b5c1d70636f677f5574c4c96
dohongthinh <dohongthinhdht1999@gmail.com>
2019-10-02 19:24:06 -0400

    Finish a1

---
+++ Week4/Assignment1-300973885/src/com/spring/mvc/YogaController.java
@@ -0,0 +1,70 @@
+package com.spring.mvc;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class YogaController {
+	//mapping with index.jsp
+	/*
+		@RequestMapping("/registration")  
+		  //method to process login request
+		   public ModelAndView registration(HttpServletRequest request,HttpServletResponse response) {
+			  String name=request.getParameter("name");
+			  String email=request.getParameter("email");
+			  String contactNumber=request.getParameter("contactNumber");
+			  String age=request.getParameter("age");
+			  String city=request.getParameter("city");
+			  String gender=request.getParameter("gender");
+			  String batch=request.getParameter("batch");
+			  String message;
+		     	
+		      message = "Hi " +name + ".";
+		      return new ModelAndView("success_registration"); 
+      }
+		
+	
+		 @RequestMapping(value = "/", method = RequestMethod.GET)
+		    public ModelAndView showForm() {
+		        return new ModelAndView("employeeHome", "employee", new YogaBean());
+		    }
+		 */
+		   @RequestMapping(value = "/registration")
+		   public ModelAndView registration(
+				   @RequestParam(value = "name")String name,
+				   @RequestParam(value = "email")String email,
+				   @RequestParam(value = "contactNumber")String contactNumber,
+				   @RequestParam(value = "age")String age,
+				   @RequestParam(value = "city")String city,
+				   @RequestParam(value = "gender")String gender,
+				   @RequestParam(value = "batch")String batch,
+		   		   @RequestParam(value = "yogaType")String[] yogaTypes){	
+			   ModelAndView mview = new ModelAndView("success_registration");
+			   String yogaType = "";
+				mview.addObject("name", name);
+				mview.addObject("email", email);
+				mview.addObject("contactNumber", contactNumber);
+				mview.addObject("age", age);
+				mview.addObject("city", city);
+				mview.addObject("gender", gender);
+				mview.addObject("batch", batch);				
+				for(int i=0;i< yogaTypes.length;i++)
+				{
+					yogaType = yogaType+ yogaTypes[i];
+					if(i < yogaTypes.length - 1)
+					{
+						yogaType = yogaType+ ", ";
+					}
+				}
+				mview.addObject("yogaType",yogaType);
+				return mview;
+		    }
+}
